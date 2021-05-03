31b80a6db39129bbe9eb49eeb237ae9da7692921
Katja <katharina.k.2019@gmail.com>
2021-05-03 11:22:19 +0800

    first commit

---
+++ anfangen/js/jscript.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const answers = [];
+answers[0] = prompt('Wie heissen Sie?' , ' ');
+answers[1] = prompt('Wie alt sind Sie?' , ' ');
+answers[2] = prompt('Woher kommen Sie?' , ' ');
+
+console.log(typeof(answers));
+console.log(typeof(null));
